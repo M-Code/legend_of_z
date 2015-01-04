33a41895c04b423bc69820a547db5c75e81727ac
Ming Liu <nongnongd@yahoo.com>
2015-01-04 18:48:34 -0500

    Added Darwin OS check

--- compile.sh
+++ compile.sh
@@ -1,6 +1,8 @@
 mkdir -p bin 
 if [ `uname` == "Linux" ]; then
     g++ src/*.cpp -lSDL2 -o bin/legend_of_z
-else
+elif [ `uname` == "Darwin" ]; then
     g++ src/*.cpp -framework SDL2 -o bin/legend_of_z
+else
+    echo "Unrecognized OS: `uname`!"
 fi
