6a63e700a33d0f9e9880e8e581fdcd7b2cdf9d44
Ming Liu <nongnongd@yahoo.com>
2015-01-08 22:34:54 -0500

    Added frame rate control

--- src/z_main.cpp
+++ src/z_main.cpp
@@ -27,19 +27,26 @@ Class declaration starts with public then protected, then private.
 /* End of Includes */
 
 /* Global Variables */
-bool running;
-SDL_Window*       mainWindow;
-Z_ScreenManager*  screenManager;
+static bool running;
+static SDL_Window*       mainWindow;
+static Z_ScreenManager*  screenManager;
 
-SDL_Renderer*     renderer;
+static SDL_Renderer*     renderer;
 
-SDL_Surface*      backgroundSurfaceL;
+static SDL_Surface*      backgroundSurfaceL;
 
-TTF_Font* fpsInfoFont;
-SDL_Color fpsInfoFontColor; 
+static TTF_Font* fpsInfoFont;
+static SDL_Color fpsInfoFontColor; 
 
-int targetFps;
-long framesRendered;
+static Uint32 targetFps;
+static Uint32 framesRendered;
+static Uint32 gameReadyTime;
+static Uint32 lastTicks;
+static Uint32 ticksPerFrame;
+static Uint32 lastSecond; // Counter FPS every second.
+static Uint32 actualFps;
+static Uint32 frameCounter;
+
 SDL_DisplayMode currentDisplayMode;
 std::ostringstream ss;
 /* End of Global Variables */
@@ -71,7 +78,6 @@ void Z_Init(void) {
 
     fpsInfoFont = Z_ResourceLoader::Z_GetInstance()->LoadTTFFont(Z_FPS_INFO_FONT_TYPE, Z_FPS_INFO_FONT_SIZE);
     fpsInfoFontColor = Z_FPS_INFO_FONT_COLOR;
-    framesRendered = 0;
 
     SDL_GetCurrentDisplayMode(0, &currentDisplayMode);
 
@@ -81,6 +87,10 @@ void Z_Init(void) {
     } else {
         targetFps = currentDisplayMode.refresh_rate;
     }
+
+    // Testing how fast can we actually render! 
+    //targetFps = 3000;
+
 }
 
 // Release all reosources.
@@ -101,18 +111,22 @@ void Z_RenderGame(void) {
     SDL_RenderPresent(renderer);
 
     framesRendered++;
+    frameCounter++;
 }
 
 void Z_RenderFPSInfo(void) {
     ss.clear();
     ss.str("");
-    ss << (framesRendered * 1000 / SDL_GetTicks() ) << " Target: " << targetFps;
+
+    ss << "Average FPS: " << (framesRendered * 1000 / (SDL_GetTicks() - gameReadyTime) ) << " Target FPS: " << targetFps << " Actual FPS: " << actualFps;
+
     Z_RenderNewTextAt(0, 0, ss.str().c_str(), fpsInfoFont, fpsInfoFontColor, renderer);
 }
 
 int main(void) {
     Z_Init();
     SDL_Event event;
+
     while (running) {
         while (SDL_PollEvent(&event)) {
             switch (event.type) {
@@ -128,10 +142,31 @@ int main(void) {
             }
         }
 
-        SDL_Delay(5);
-
         Z_UpdateGame();
+
+        if ( lastTicks != 0 ) {
+            Uint32 ticksGone = SDL_GetTicks() - lastTicks;
+            if(ticksGone < ticksPerFrame) {
+                SDL_Delay(ticksPerFrame - ticksGone);
+            }
+        } else {
+            // Initialize our metrics.
+            lastSecond = SDL_GetTicks();
+            gameReadyTime = SDL_GetTicks();
+        }
+
         Z_RenderGame();
+
+
+        lastTicks = SDL_GetTicks(); // Start Timing right after rendering.
+        if ( lastTicks - lastSecond >= 1000 ) {
+            lastSecond = lastTicks;
+            actualFps = frameCounter;
+            frameCounter = 0;
+        }
+
+
+        ticksPerFrame = (1000 - (lastTicks -lastSecond)) / (targetFps - frameCounter) ;
     }
 
     Z_Destroy();   
